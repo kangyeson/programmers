ae6ec20bc86c0ed29903ed97fd885315cd00d5e9
Yeson Kang <yesonkang@gmail.com>
2025-03-08 23:29:16 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,3 +1,2 @@
 def solution(a, b):
-    answer = lamda x : int(str(a)+str(b)) if int(str(a)+str(b)) >= int(str(b)+str(a)) else int(str(b)+str(a))
-    return answer
+    return int(max(f'{a}{b}', f'{b}{a}'))
