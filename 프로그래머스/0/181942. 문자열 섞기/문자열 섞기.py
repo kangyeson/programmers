be391bb0f92b52f98b33db5483aec1ea90f10472
Yeson Kang <yesonkang@gmail.com>
2025-03-08 23:08:39 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -1,10 +1,7 @@
 def solution(str1, str2):
     answer = ''
-    while len(str1) + len(str2) > 0:
-        if len(str1) >= 1:
-            answer = answer + str1[0]
-            str1 = str1[1:]
-        if len(str2) >= 1:
-            answer = answer + str2[0]
-            str2 = str2[1:]
+    while len(str1)+len(str2) > 0:
+        answer = answer+str1[0]+str2[0]
+        str1.lstrip()
+        str2.lstrip()
     return answer
