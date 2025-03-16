7b206f088deda94460b1a37fd2de1b8ef73fa446
Yeson Kang <yesonkang@gmail.com>
2025-03-16 17:03:53 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 2.16 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -1,8 +1,5 @@
 def solution(my_string, queries):
-    for a, b in queries:
-        ChangeList = list(my_string[a:b+1])
-        ChangeList.reverse()
-        start = lambda a: my_string[0:a] if a >= 0 else ''
-        end = lambda b: my_string[b+1:len(my_string)] if b+1<=len(my_string) else ''
-        my_string = start(a) + ''.join(ChangeList) + end(b)
-    return my_string
+    answer = list(my_string)
+    for s,e in queries:
+        answer[s:e+1] = answer[s:e+1][::-1]
+    return ''.join(answer)
