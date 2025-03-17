0450dee26fdfc5dbed2c36e42eb4c106a65d441e
Yeson Kang <yesonkang@gmail.com>
2025-03-17 16:47:19 +0900

    [level 0] Title: 접미사 배열, Time: 0.04 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
@@ -1,4 +1,2 @@
 def solution(my_string):
-    answer = [my_string[-i:] for i in range(int(len(my_string)+1/2))]
-    answer.sort()
-    return answer
+    return sorted(my_string[i:] for i in range(len(my_string)))
