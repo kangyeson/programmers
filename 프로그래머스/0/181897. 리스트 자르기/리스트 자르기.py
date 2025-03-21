f7939b9671a0a3824d05677782b853743c4b3831
Yeson Kang <yesonkang@gmail.com>
2025-03-21 15:34:04 +0900

    [level 0] Title: 리스트 자르기, Time: 0.00 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.py
@@ -1,11 +1,10 @@
 def solution(n, slicer, num_list):
-    answer = []
+    a, b, c = slicer
     if n == 1:
-        answer = num_list[0:slicer[1]+1]
+        return num_list[0:b+1]
     elif n == 2:
-        answer = num_list[slicer[0]:]
+        return num_list[a:]
     elif n == 3:
-        answer = num_list[slicer[0]:slicer[1]+1]
+        return num_list[a:b+1]
     else:
-        answer = num_list[slicer[0]:slicer[1]+1:slicer[2]]
-    return answer
+        return num_list[a:b+1:c]
