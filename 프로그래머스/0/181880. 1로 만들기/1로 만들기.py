5ee22b55027c400f48b9cecba99a284db5f2b470
Yeson Kang <yesonkang@gmail.com>
2025-03-24 17:29:49 +0900

    [level 0] Title: 1로 만들기, Time: 0.02 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -2,10 +2,6 @@ def solution(num_list):
     answer = 0
     for v in num_list:
         while v != 1:
-            if v%2==0:
-                v = int(v/2)
-                answer+=1
-            else:
-                v = int((v-1)/2)
-                answer+=1
+            v //= 2
+            answer += 1
     return answer
