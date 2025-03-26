41ae98546cc7c10a397633dc2f88355472bcba20
Yeson Kang <yesonkang@gmail.com>
2025-03-26 19:36:29 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.08 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.py
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.py
@@ -1,4 +1,2 @@
 def solution(my_string):
-    my_string = my_string.strip()
-    my_string = my_string.split(' ')
-    return [i for i in my_string if i!=""]
+    return [i for i in my_string.split(" ") if i != ""]
