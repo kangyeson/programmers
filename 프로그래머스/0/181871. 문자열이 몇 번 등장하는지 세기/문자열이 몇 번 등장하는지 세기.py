ba35c043f984fffc19cef5bb36432073ecbd353c
Yeson Kang <yesonkang@gmail.com>
2025-03-26 19:10:58 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.42 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
@@ -1,7 +1,2 @@
 def solution(myString, pat):
-    answer = 0
-    for i in range(len(myString)):
-        if myString[i:i+len(pat)] == pat:
-            answer+=1
-        
-    return answer
+    return sum(myString[i:i+len(pat)] == pat for i in range(len(myString)))
