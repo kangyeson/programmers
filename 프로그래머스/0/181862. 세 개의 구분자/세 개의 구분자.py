856b6a255218340f9e8137492bff8ff679558621
Yeson Kang <yesonkang@gmail.com>
2025-03-27 18:57:34 +0900

    [level 0] Title: 세 개의 구분자, Time: 8.35 ms, Memory: 22.6 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,7 +1,2 @@
 def solution(myStr):
-    a = str.maketrans("abc", "   ")
-    myStr = myStr.translate(a).strip()
-    if myStr == "":
-        return ["EMPTY"]
-    else:
-        return myStr.split()
+    return myStr.split("a").split("b").split("c")
