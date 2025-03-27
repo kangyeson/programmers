5155aa58044cb6d2658c5fe47443f81df2791ed8
Yeson Kang <yesonkang@gmail.com>
2025-03-27 19:44:01 +0900

    [level 0] Title: 배열 만들기 6, Time: 108.67 ms, Memory: 17.2 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -1,17 +1,8 @@
 def solution(arr):
     stk = []
-    i = 0
-    while i<len(arr):
-        if not stk:
-            stk.append(arr[i])
-            i+=1
+    for i in range(len(arr)):
+        if stk and stk[-1]==arr[i]:
+            stk.pop()
         else:
-            if stk[-1] == arr[i]:
-                del stk[-1:]
-                i+=1
-            else:
-                stk.append(arr[i])
-                i+=1
-    if not stk:
-        return [-1]
-    return stk
+            stk.append(arr[i])
+    return stk or [-1]
