1b68e63ff508b004ecd543946e891046e5de0ded
Yeson Kang <yesonkang@gmail.com>
2025-03-28 13:27:13 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 2572.64 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -1,8 +1,4 @@
 def solution(arr, k):
     answer=[]
     [answer.append(i) for i in arr if not i in answer]
-    if len(answer[:k]) != k:
-        [answer.append(-1) for i in range(k-len(answer))]
-        return answer
-    else:
-        return answer[:k]
+    return answer[:k] + [-1] * (k-len(answer))
