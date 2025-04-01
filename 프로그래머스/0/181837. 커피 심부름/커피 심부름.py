1259cd0415db6396eed550601e3d21b4a287b51b
Yeson Kang <yesonkang@gmail.com>
2025-04-01 09:21:22 +0900

    [level 0] Title: 커피 심부름, Time: 0.06 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -1,10 +1,2 @@
 def solution(order):
-    answer = 0
-    for i in order:
-        if "americano" in i:
-            answer += 4500
-        elif "latte" in i:
-            answer += 5000
-        else:
-            answer += 4500
-    return answer
+    return sum([5000 if 'latte' in i else 4500 for i in order])
