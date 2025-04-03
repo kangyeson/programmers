fc180174be71e02ead2264d6a56cf819938bc04e
Yeson Kang <yesonkang@gmail.com>
2025-04-03 19:41:52 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.04 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -1,28 +1,14 @@
 def solution(a, b, c, d):
-    answer = 1111*a
-    dice = [a, b, c, d]
-    equlnum = {}
-    for i in dice:
-        if i in equlnum:
-            equlnum[i]+=1
+    dice = [a,b,c,d]
+    c = [dice.count(x) for x in dice] 
+    if max(c) == 4:
+        return 1111*a
+    elif max(c) == 3:
+        return (10*dice[c.index(3)]+ dice [c.index(1)])**2
+    elif max(c) == 2:
+        if min(c) == 1:
+            return eval('*'.join([str(dice[i]) for i, x in enumerate(c) if x == 1]))
         else:
-            equlnum[i]=1
-    
-    for key, value in equlnum.items():
-        diffnum = [k for k, v in equlnum.items() if key != k]
-        if value == 3:
-            answer = (10 * key + diffnum[0])**2
-            break
-            
-        elif value == 2:
-            if len(diffnum)==1:
-                answer = (key + diffnum[0]) * abs(key-diffnum[0])
-                break
-            else:
-                answer = diffnum[0]*diffnum[1]
-                break
-            
-        elif value == 1:
-            answer = min(dice)
-        
-    return answer
+            return (max(dice) + min(dice)) * abs(max(dice) - min(dice))
+    else:
+        return min(dice)
